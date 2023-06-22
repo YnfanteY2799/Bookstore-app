6c4858c1b16677100c11523818c98bac51ea4caa
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2023-06-22 10:50:06 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import { routes } from "@/routes";
 
 import type { ReactElement } from "react";
 
 export default function App(): ReactElement {
+  // Create a client
+  const queryClient = new QueryClient();
+
   // Hook Router
   const router = createBrowserRouter(routes);
 
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
